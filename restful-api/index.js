c94964bf8926671392bb5f8a7c5295633e6c1ffd
Ricardo Alves <ricardospalves@gmail.com>
2024-09-25 00:34:31 -0300

    feat(restful-api): create server

--- restful-api/index.js
+++ restful-api/index.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+import { createServer } from "node:http";
+
+const PORT = 3000;
+
+const server = createServer((request, response) => {
+  return response.end("Hello World!");
+});
+
+server.listen(PORT, () => {
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
