dbbfc7456ed2baae1cb8ee7b2050550280a801d6
Ricardo Alves <ricardospalves@gmail.com>
2024-09-26 00:37:28 -0300

    feat(restful-api): parse request paths

--- restful-api/index.js
+++ restful-api/index.js
@@ -1,8 +1,19 @@
 import { createServer } from "node:http";
+import url from "node:url";
 
 const PORT = 3000;
 
 const server = createServer((request, response) => {
+  // Get the URL and parse it
+  const parsedUrl = url.parse(request.url, true);
+
+  // Get the path
+  const pathname = parsedUrl.pathname;
+  const trimedPathname = pathname.replace(/^\/+|\/+$/g, "");
+
+  // Log the request path
+  console.log("Request received on path:", trimedPathname);
+
   return response.end("Hello World!");
 });
 
