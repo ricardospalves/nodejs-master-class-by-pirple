719c92d8611d95ededd0156a5c04486512f34c54
Ricardo Alves <ricardospalves@gmail.com>
2024-10-02 00:33:57 -0300

    feat(restful-api): add route requests

--- restful-api/index.js
+++ restful-api/index.js
@@ -4,13 +4,36 @@ import { StringDecoder } from "string_decoder";
 
 const PORT = 3000;
 
+// Define the handlers
+const handlers = {};
+
+// Sample handler
+handlers.sample = (data, callback) => {
+  // Callback a http status code and a payload object
+  callback(406, {
+    name: "sample handler",
+  });
+};
+
+// Not found handler
+handlers.notFound = (data, callback) => {
+  callback(404, {
+    message: "Route not found.",
+  });
+};
+
+// Define a request router
+const router = {
+  sample: handlers.sample,
+};
+
 const server = createServer((request, response) => {
   // Get the URL and parse it
   const parsedUrl = url.parse(request.url, true);
 
   // Get the path
   const pathname = parsedUrl.pathname;
-  const trimedPathname = pathname.replace(/^\/+|\/+$/g, "");
+  const trimmedPathname = pathname.replace(/^\/+|\/+$/g, "");
 
   // Get the query string as an object
   const queryStringObject = parsedUrl.query;
@@ -33,13 +56,45 @@ const server = createServer((request, response) => {
     buffer += decoder.end();
 
     // Log the request path
-    console.log("Pathname:", trimedPathname);
+    console.log("Pathname:", trimmedPathname);
     console.log("Method:", method);
     console.log("Query:", queryStringObject);
     console.log("Headers:", headersObject);
     console.log("Payload:", buffer);
 
-    return response.end("Hello World!");
+    // Choose the handler this request should go to. Is one is not found, use the notFound handler
+    const chosenHandler = router[trimmedPathname]
+      ? router[trimmedPathname]
+      : handlers.notFound;
+
+    console.log("[chosen]", chosenHandler);
+
+    // Construct the data object to send to the handler
+    const data = {
+      trimmedPathname,
+      queryStringObject,
+      method,
+      headersObject,
+      payload: buffer,
+    };
+
+    // Route the request to the handler specified in the router
+    chosenHandler(data, function (statusCode, payload) {
+      // Use the statusCode called back by the handler, or default to 200
+      statusCode = typeof statusCode === "number" ? statusCode : 200;
+
+      // Use the payload called bu the handler, or default to an empty object
+      payload = typeof payload === "object" ? payload : {};
+
+      // Convert the payload to a string
+      const payloadString = JSON.stringify(payload);
+
+      // Return the response
+      response.writeHead(statusCode);
+      response.end(payloadString);
+
+      console.log("Returning this response:", statusCode, payloadString);
+    });
   });
 });
 
