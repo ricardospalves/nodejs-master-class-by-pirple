fb602f57bf56510f3978719fcdc9c10685179be4
Ricardo Alves <ricardospalves@gmail.com>
2025-02-20 00:54:47 -0300

    fex(restful-api): fix tokenData method name

--- restful-api/lib/router/handlers/tokens.js
+++ restful-api/lib/router/handlers/tokens.js
@@ -191,7 +191,7 @@ export const verifyToken = (id, phone, callback) => {
     }
 
     // Check that the token is for the given user and has not expired
-    if (tokenData.phone !== phone || tokenData.eexpires <= Date.now()) {
+    if (tokenData.phone !== phone || tokenData.expires <= Date.now()) {
       callback(false);
     }
 
