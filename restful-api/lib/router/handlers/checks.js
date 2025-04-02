9fd46b518be44a0496d2c9f6a4032899bf5836c0
Ricardo Alves <ricardospalves@gmail.com>
2025-04-02 00:37:44 -0300

    feat(restful-api): add checks put endpoint

--- restful-api/lib/router/handlers/checks.js
+++ restful-api/lib/router/handlers/checks.js
@@ -121,8 +121,6 @@ const _checks = {
   /**
    * Checks - GET
    *
-
-   *
    * @param {Object} data
    * @param {string} data.id id of check
    * @param {errorCallbak} callback
@@ -166,7 +164,132 @@ const _checks = {
     });
   },
 
-  put() {},
+  /**
+   * Checks - PUT
+   *
+   * @callback callback
+   * @param {number} statusCode
+   * @param {Object} responseData
+   *
+   * @param {Object} data
+   * @param {Object} data.payload
+   * @param {string} data.payload.id
+   * @param {string} [data.payload.protocol]
+   * @param {string} [data.payload.url]
+   * @param {string} [data.payload.method]
+   * @param {number} [data.payload.successCodes]
+   * @param {number} [data.payload.timeoutSeconds]
+   * @param {callback} callback
+   */
+  put(data, callback) {
+    const payload = data.payload;
+
+    // Check for the required field
+    const id =
+      typeof payload.id === "string" && payload.id.trim().length === 20
+        ? payload.id.trim()
+        : false;
+
+    // Validate inputs
+    const protocol =
+      typeof payload?.protocol === "string" &&
+      /^http(s?)$/.test(payload.protocol)
+        ? payload.protocol
+        : false;
+
+    const url =
+      typeof payload?.url === "string" && payload.url.trim().length
+        ? payload.url.trim()
+        : false;
+
+    const method =
+      typeof payload?.method === "string" &&
+      /^post|get|put|delete|$/.test(payload.method)
+        ? payload.method
+        : false;
+
+    const successCodes =
+      Array.isArray(payload?.successCodes) && payload.successCodes.length
+        ? payload.successCodes
+        : false;
+
+    const timeoutSeconds =
+      typeof payload?.timeoutSeconds === "number" &&
+      payload.timeoutSeconds % 1 === 0 &&
+      payload.timeoutSeconds >= 1 &&
+      payload.timeoutSeconds <= 5
+        ? payload.timeoutSeconds
+        : false;
+
+    // Check to make sure id is valid.
+    if (!id) {
+      return callback(400, {
+        error: "Missing required field.",
+      });
+    }
+
+    // Check to make sure one or more optional fields has been sent.
+    if (!Boolean(protocol || url || method || successCodes || timeoutSeconds)) {
+      return callback(400, {
+        error: "Missing fields to update.",
+      });
+    }
+
+    _data.read("checks", id, (error, checkData) => {
+      if (error || !checkData) {
+        return callback(400, {
+          error: "Check ID did not exist.",
+        });
+      }
+
+      // Get the token from the HEADERS
+      const token =
+        typeof data.headers?.token === "string" ? data.headers.token : false;
+
+      // Verify that the given token is valid and belongs to the user who
+      // created check.
+      return verifyToken(token, checkData.userPhone, (tokenIsValid) => {
+        if (!tokenIsValid) {
+          return callback(403, {
+            error: "Missing required token in headers or token is invalid.",
+          });
+        }
+
+        // Update the check where necessary.
+        if (protocol) {
+          checkData.protocol = protocol;
+        }
+
+        if (url) {
+          checkData.url = url;
+        }
+
+        if (method) {
+          checkData.method = method;
+        }
+
+        if (successCodes) {
+          checkData.successCodes = successCodes;
+        }
+
+        if (timeoutSeconds) {
+          checkData.timeoutSeconds = timeoutSeconds;
+        }
+
+        // Store the new updates
+        return _data.update("checks", id, checkData, (error) => {
+          if (error) {
+            return callback(500, {
+              error: "Could not update the check.",
+            });
+          }
+
+          return callback(200);
+        });
+      });
+    });
+  },
+
   delete() {},
 };
 
